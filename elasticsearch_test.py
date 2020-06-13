74d6daca27e7967db4c5c2ccc0757a6006cb6f8a
Isaac Keleher <isaac.keleher@gmail.com>
2020-06-13 15:39:59 +1000

    Comments and tidying up code

--- elasticsearch_test.py
+++ elasticsearch_test.py
@@ -4,28 +4,27 @@ import requests
 import json
 from requests_aws4auth import AWS4Auth
 
+# set credentials 
 region = 'ap-southeast-2' # e.g. us-west-1
 service = 'es'
 credentials = boto3.Session().get_credentials()
 awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
 
-host = 'https://search-plantly-es-cheap-my4i72dmshwihajjj2sbwqii3i.ap-southeast-2.es.amazonaws.com' # the Amazon ES domain, including https://
+# set elasticsearch cluster, the Amazon ES domain, including https://
+host = 'https://search-plantly-es-cheap-my4i72dmshwihajjj2sbwqii3i.ap-southeast-2.es.amazonaws.com' 
 
+# headers for http equest
 headers = { "Content-Type": "application/json" }
-
-s3 = boto3.client('s3')
 
 # Regular expressions used to parse some simple log lines
 ip_pattern = re.compile('(\d+\.\d+\.\d+\.\d+)')
 time_pattern = re.compile('\[(\d+\/\w\w\w\/\d\d\d\d:\d\d:\d\d:\d\d\s-\d\d\d\d)\]')
 message_pattern = re.compile('\"(.+)\"')
 
+# query to test if cluster is returning data
 type = "user_data"
-
 _id =  'c0cb03a49a754a17b07b85c4d4f19039-test'
-
 url = host + '/' + _id + '/' + type
-
 
 query_body = {
   "query": {
@@ -33,6 +32,6 @@ query_body = {
   }
 }
 
+# sent and print if succesfful
 r = requests.post(url, auth=awsauth, json=query_body, headers=headers)
-        
 print(r.text)
